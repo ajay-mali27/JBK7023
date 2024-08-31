0f21d027856d858a068648af399340c357886cc5
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-08-31 09:53:26 +0530

    with 3rd variable

--- Coding Questions/src/P1_SwappingOfNoWithThirdVeriable.java
+++ Coding Questions/src/P1_SwappingOfNoWithThirdVeriable.java
@@ -13,6 +13,7 @@ public class P1_SwappingOfNoWithThirdVeriable {
 		temp = a; // temp=10;
 		a = b; // a=20;
 		b = temp;
+		
 		System.out.println("**************");
 		System.out.println("Value of a :" + a);
 		System.out.println("Value of b :" + b);
