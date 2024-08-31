7a85d7a921a00b34813097d423e34bbbde947e19
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-08-31 09:30:59 +0530

    1 Swapping Of No With Third Veriable

---
+++ Coding Questions/src/P1_SwappingOfNoWithThirdVeriable.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P1_SwappingOfNoWithThirdVeriable {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int temp;
+
+		System.out.println("Before Swapping");
+		System.out.println("The Value of a :" + a);
+		System.out.println("The Value of b :" + b);
+
+		temp = a; // temp=10;
+		a = b; // a=20;
+		b = temp;
+		System.out.println("**************");
+		System.out.println("Value of a :" + a);
+		System.out.println("Value of b :" + b);
+	}
+}
